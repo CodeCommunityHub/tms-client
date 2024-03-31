c3dbc58b005f66e242806765ad794d6fef84d0bb
anuradha fernando <anufdo@gmail.com>
2024-03-31 21:02:35 +0530

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-function App() {
-  return <div>Training Management Software</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="bg-gray-100 flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-indigo-600 mb-8">Training Management Software</h1>
+    </div>
+
+  );
+};
+
 
 export default App;
