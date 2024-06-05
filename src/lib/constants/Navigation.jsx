909a8165aeb9a99d309758b4fbf2c04ce05d9e4d
Anuradha Fernando <anufdo@gmail.com>
2024-06-05 10:41:13 +0530

    add side bar

---
+++ src/lib/constants/Navigation.jsx
@@ -0,0 +1,58 @@
+import {
+  HiOutlineAnnotation,
+  HiOutlineCog,
+  HiOutlineCube,
+  HiOutlineDocumentText,
+  HiOutlineQuestionMarkCircle,
+  HiOutlineShoppingCart,
+  HiOutlineUsers,
+  HiOutlineViewGrid,
+} from "react-icons/hi";
+
+export const DASHBOARD_SIDEBAR_LINKS = [
+  {
+    key: "dashboard",
+    label: "Dashboard",
+    path: "/",
+    icon: <HiOutlineViewGrid />,
+  },
+  {
+    key: "students",
+    label: "Students",
+    path: "/students",
+    icon: <HiOutlineCube />,
+  },
+  {
+    key: "courses",
+    label: "Courses",
+    path: "/courses",
+    icon: <HiOutlineShoppingCart />,
+  },
+  {
+    key: "instructors",
+    label: "Instructors",
+    path: "/instructors",
+    icon: <HiOutlineUsers />,
+  },
+  {
+    key: "messages",
+    label: "Messages",
+    path: "/messages",
+    icon: <HiOutlineAnnotation />,
+  },
+];
+
+export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
+  {
+    key: "settings",
+    label: "Settings",
+    path: "/settings",
+    icon: <HiOutlineCog />,
+  },
+  {
+    key: "support",
+    label: "Help & Support",
+    path: "/support",
+    icon: <HiOutlineQuestionMarkCircle />,
+  },
+];
